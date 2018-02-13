f5850e6a9250d08cf21c9644c6e97bdcfabe1fb2
Gavin Gong <kefengong@gmail.com>
2018-02-13 12:55:00 +0800

    update

--- src/diff.js
+++ src/diff.js
@@ -1,9 +1,28 @@
+import { REPLACE } from './constant'
+
 class Diff {
   constructor(newTree, oldTree) {
-
+    this.newTree = newTree
+    this.oldTree = oldTree
   }
   diff() {
+    console.log(this.newTree, this.oldTree)
+    let { newTree, oldTree } = this
+    let index = 0
+    let patches = {}
+    
+    /* diff tagName*/
+    if (newTree.tag !== oldTree.tag) {
+      pactches[ ++index ] = {
+          type: REPLACE,
+          node: document.createElement() 
+        }
+      }
+    
+    /* diff props */
 
+    /* diff children */
+    
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Ele from './h'
-
+import { diff } from './diff'
 const node = {
   tag: 'ul',
   props: {
@@ -12,5 +12,24 @@ const node = {
   ]
 }
 
+const node2 = {
+  tag: 'div',
+  props: {
+    class: 'name'
+  },
+  children: [
+    { tag: 'li', props: { class: 'item' }, children: "hh" },
+    { tag: 'li', props: { class: 'item' }, children: "33" },
+    { tag: 'li', props: { class: 'item' }, children: "99" },
+  ]
+}
+
+
 const app = new Ele(node.tag, node.props, node.children)
 app.$mount('#app')
+
+
+const oldTree = app.VNode
+const newTree = new Ele(node2.tag, node2.props, node2.children).VNode
+
+const diffTree = diff(newTree, oldTree)
